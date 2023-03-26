299782d50f061b7695de5d63f571aad675900d1f
Muhammad Ahsan <99596265+MuhammedAhsan@users.noreply.github.com>
2023-03-26 19:36:30 +0500

    complete simple calculator using classes
    Simple calculator completed, now onto a different approach

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,52 +1,86 @@
 //Complete scientific calculator
 
+//Basic Calculator (+ - * /)								Done
+//Add tregnomatric signs (sin, cos, tan, etc)				
+//write like a normal calculator (cin one after another)	
+
+
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 using namespace std;
 
 class Calculate{
 	private:
 		int num1;
-		int num2;
-		char sign;
 	public:
-		Calculate(){num1 = 0; num2 = 0, sign = ' ';};
-		Calculate(int firstNum, int secondNum, char numSign){num1 = firstNum; num2 = secondNum, sign = numSign;};
-		void calculated(){
-			if(sign == '+'){cout<<"Answer: "<<num1<<" "<<sign<<" "<<num2<<" = "<<num1 + num2;}
-			else if(sign == '-'){cout<<"Answer: "<<num1<<" "<<sign<<" "<<num2<<" = "<<num1 - num2;}
-			else if(sign == '*' || sign == 'x'){cout<<"Answer: "<<num1<<" "<<sign<<" "<<num2<<" = "<<num1 * num2;}
-			else if(sign == '/'){cout<<"Answer: "<<num1<<" "<<sign<<" "<<num2<<" = "<<num1 / num2;}
-			else {cout<<"Invalid Sign";}
-		}
+		Calculate(){num1 = 0;};
+		Calculate(int firstNum){num1 = firstNum;};
+		
+		Calculate operator +(const Calculate & num);
+		Calculate operator -(const Calculate & num);
+		Calculate operator *(const Calculate & num);
+		Calculate operator /(const Calculate & num);
+		friend Calculate operator <<(ostream & os, const Calculate & num);
+		friend Calculate operator >>(istream & is, const Calculate & num);
 };
+
+Calculate Calculate::operator +(const Calculate & num){
+	Calculate t;
+	t.num1 = num1 + num.num1;
+	return t;
+}
+Calculate Calculate::operator -(const Calculate & num){
+	Calculate t;
+	t.num1 = num1 - num.num1;
+	return t;
+}
+Calculate Calculate::operator *(const Calculate & num){
+	Calculate t;
+	t.num1 = num1 * num.num1;
+	return t;
+}
+Calculate Calculate::operator /(const Calculate & num){
+	Calculate t;
+	t.num1 = num1 / num.num1;
+	return t;
+}
+Calculate operator <<(ostream & os, const Calculate & num){
+	os<<num.num1;
+}
+Calculate operator >>(istream & is, const Calculate & num){
+	is>>num.num1;
+}
 
 int main(){
 	int num1, num2;
 	char sign, flag = 'y';
 	
-	while(flag == 'y' || flag == 'Y'){
-		cout<<"Enter First number: ";
-		cin>>num1;
-		cout<<"Enter Second number: ";
-		cin>>num2;
-		cout<<"Calculate (+, -, *, /): ";
-		cin>>sign;
-		cout<<endl;
-		
-		Calculate fNum(num1, num2, sign);
-		fNum.calculated();
-		cout<<endl<<endl;
-		
-		int innerFlag = 1;
-		while(innerFlag == 1){
-			cout<<"Do you want to Calculate more (Y/N): ";
-			cin>>flag;
-			if(flag == 'y' || flag == 'Y'){flag = 'y'; innerFlag = 0; system("CLS");}
-			else if(flag == 'n' || flag == 'N'){flag = 'n'; innerFlag = 0;}
-			else {cout<<"Invalid!"<<endl; flag = 'y'; innerFlag = 1;}
-		}
-	}
+	Calculate numb1(45), numb2(1);
+	cout<<numb1 + numb2;
+	
+//	while(flag == 'y' || flag == 'Y'){
+//		cout<<"Enter First number: ";
+//		cin>>num1;
+//		cout<<"Enter Second number: ";
+//		cin>>num2;
+//		cout<<"Calculate (+, -, *, /): ";
+//		cin>>sign;
+//		cout<<endl;
+//		
+//		Calculate fNum(num1, num2, sign);
+//		fNum.calculated();
+//		cout<<endl<<endl;
+//		
+//		int innerFlag = 1;
+//		while(innerFlag == 1){
+//			cout<<"Do you want to Calculate more (Y/N): ";
+//			cin>>flag;
+//			if(flag == 'y' || flag == 'Y'){flag = 'y'; innerFlag = 0; system("CLS");}
+//			else if(flag == 'n' || flag == 'N'){flag = 'n'; innerFlag = 0;}
+//			else {cout<<"Invalid!"<<endl; flag = 'y'; innerFlag = 1;}
+//		}
+//	}
 	
 	
 }
